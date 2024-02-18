8c44b5aca5c4ffe6e72bee4a0fdeaeda1cae79da
lisperey <pilrss16@gmail.com>
2024-02-18 12:55:23 -0300

    Finalizando backend da aplicação

--- back-end/cardapio/src/main/java/com/example/cardapio/food/Food.java
+++ back-end/cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Table(name = "foods")
 @Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
     @jakarta.persistence.Id
     @Id
@@ -16,4 +24,10 @@ public class Food {
     private String title;
     private String image;
     private Integer price;
+
+    public Food(FoodRequestDTO data){
+        this.image = data.image();
+        this.title = data.title();
+        this.price = data.price();
+    }
 }
